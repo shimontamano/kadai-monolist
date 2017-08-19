16f252d45e8eb9d0cdb50200f3e7248d4532b054
Shimon Tamano <shimogramming@gmail.com>
2017-08-19 12:06:59 +0000

    want

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
+  
+  resources :items, only: [:new, :show]
+  resources :ownerships, only: [:create, :destroy]
 end
